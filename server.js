cf6f1eaec8d5da59cc2034553ff9ab15b8992175
Developers Today <mshari7185@gmail.com>
2023-09-08 20:57:07 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express()
+
+
+
+///routes
+
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+
+
+
+
+
+
+app.listen(3000, () =>{
+  console.log("App is running");
+})
